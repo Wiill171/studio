520439baf54f134598f3c71de790d9f5c733c44a
Ildefonso Fernandes <IldefonsoFernandes6@gmail.com>
2025-10-17 22:40:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9470503834-7c0d6",
+  "appId": "1:388851461779:web:41835c8442a6adc822e766",
+  "apiKey": "AIzaSyC0FRCA4DhlYDunOl1Wsld3kErje43saao",
+  "authDomain": "studio-9470503834-7c0d6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "388851461779"
+};
