aea773308891695a06cad6dfcf86aeb398cd41e6
Ildefonso Fernandes <IldefonsoFernandes6@gmail.com>
2025-10-23 23:58:49 +0000

    adicinar imagem como plano de fundo do aplicativo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <FirebaseClientProvider>
           <LanguageProvider>
-            <div className="flex min-h-screen w-full flex-col">
+            <div className="flex min-h-screen w-full flex-col bg-background/80 backdrop-blur-sm">
               <Header />
               <main className="flex-1">{children}</main>
             </div>
